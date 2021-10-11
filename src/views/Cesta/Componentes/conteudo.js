46bbeab4431c3a80e143c1497a96a96fde1f24d0
Elton Santos <elton.santos@unimedmaceio.com.br>
2021-10-11 16:59:18 -0300

    Add item detail

---
+++ src/views/Cesta/Componentes/conteudo.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {Image, StyleSheet, View} from 'react-native';
+
+import Texto from '../../../componentes/texto'
+
+import logo from '../../../../assets/logo.png';
+
+export default function ItemDetail(){
+
+    return <><Texto style={style.nome}>Cesta de Verduras</Texto>
+    <View style={style.fazenda}>
+        <Image source={logo} style={style.logo}></Image>
+        <Texto style={style.nomeFazenda}>Jenny Jack Farm</Texto>
+    </View>
+    
+    <Texto style={style.descricao}>Uma cesta com produtos 
+        cuidadosamente direto para 
+        sua cozinha.</Texto>
+    <Texto style={style.preco}>R$ 40,00</Texto></>
+
+}
+
+const style = StyleSheet.create({
+
+    nome:{
+        fontSize:26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold",
+        
+    },
+    fazenda:{
+        flexDirection:"row",
+        paddingVertical:12
+    },
+    logo:{
+        width: 32,
+        height:32
+    },
+    nomeFazenda:{
+        fontSize:16,
+        lineHeight:26,
+        marginLeft:12,
+        fontFamily:"MontserratRegular"
+    },
+    descricao:{
+        color:"#A3A3A3",
+        fontSize:16,
+        lineHeight:26
+    },
+    preco:{
+        color:"#2A9F85",
+        fontWeight:"bold",
+        fontSize:26,
+        lineHeight:42,
+        margimTop:8
+    },
+
+})
--- src/views/Cesta/index.js
+++ src/views/Cesta/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Image , StyleSheet, Dimensions, Text, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
-import Texto from '../../componentes/texto';
 
-import logo from '../../../assets/logo.png';
 
 import Topo from './Componentes/topo';
+import ItemDetail from './Componentes/conteudo';
 
 
 
@@ -13,16 +12,7 @@ export default function Cesta() {
     return <><Topo/>
 
     <View style={style.cesta}>
-        <Texto style={style.nome}>Cesta de Verduras</Texto>
-        <View style={style.fazenda}>
-            <Image source={logo} style={style.logo}></Image>
-            <Texto style={style.nomeFazenda}>Jenny Jack Farm</Texto>
-        </View>
-        
-        <Texto style={style.descricao}>Uma cesta com produtos 
-            cuidadosamente direto para 
-            sua cozinha.</Texto>
-        <Texto style={style.preco}>R$ 40,00</Texto>
+        <ItemDetail/>
     </View>
     </>
     
@@ -36,39 +26,7 @@ const style = StyleSheet.create({
         paddingHorizontal:16,
         
     },
-    nome:{
-        fontSize:26,
-        lineHeight: 42,
-        color: "#464646",
-        fontWeight: "bold",
-        
-    },
-    fazenda:{
-        flexDirection:"row",
-        paddingVertical:12
-    },
-    logo:{
-        width: 32,
-        height:32
-    },
-    nomeFazenda:{
-        fontSize:16,
-        lineHeight:26,
-        marginLeft:12,
-        fontFamily:"MontserratRegular"
-    },
-    descricao:{
-        color:"#A3A3A3",
-        fontSize:16,
-        lineHeight:26
-    },
-    preco:{
-        color:"#2A9F85",
-        fontWeight:"bold",
-        fontSize:26,
-        lineHeight:42,
-        margimTop:8
-    },
+    
     
 
 });
